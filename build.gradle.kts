d453ba1c98b28b991bead2e3e1706b381dfafcce
MAKEREKAM <116092283+MAKEREKAM@users.noreply.github.com>
2023-08-30 17:36:36 +0900

    복사 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 group = "kr.vanilage"
 version = "1.0" // 여기에 버전을 작성하세요.
 
+var buildPath = "" // 빌드 주소를 설정하세요.
+
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
@@ -14,18 +16,20 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT"); // 여기 버전을 바꿀수 있습니다.
 }
 
+task("copy") {
+    doLast {
+        copy {
+            from("build/libs") // 파일이 나오는 주소를 설정하세요.
+            include("*.jar")
+            into(buildPath)
+        }
+
+        delete(buildPath.plus("\\RELOAD"))
+    }
+}
+
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    copy {
-        from("build/libs")
-        into("D:\\Programming\\Mcserver\\연구소\\plugins\\update") // 여기서 빌드 주소를 설정하세요.
-        include("*.jar")
+    named("jar") {
+        finalizedBy("copy")
     }
 }
